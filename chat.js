766b02c50ecc1676a0b6fde08c487881d26756b3
duffycodes <mduff001@odu.edu>
2019-02-17 16:04:32 -0600

    added server

--- chat.js
+++ chat.js
@@ -5,4 +5,8 @@
 var express = require('express')
 var app = express()
 
-app.listen(3000)
+app.use(express.static(__dirname))
+var server = app.listen(3000, ()=>{
+	console.log('server is listening on port', server.address().port)
+})
+
