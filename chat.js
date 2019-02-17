764c1aa2ef69dba9476270aacc4ccb928b8d1fdd
duffycodes <mduff001@odu.edu>
2019-02-17 16:25:50 -0600

    added message scripts

--- chat.js
+++ chat.js
@@ -6,6 +6,16 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+	{name: 'Tim', message:'Hi'},
+	{name: 'Jamie', message:'Hello'}
+]
+
+app.get('/messages', (req, res)=>{
+	res.send(messages)
+})
+
 var server = app.listen(3000, ()=>{
 	console.log('server is listening on port', server.address().port)
 })
