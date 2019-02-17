4ccf2ea5247627a6150fc27e44b5358ed3be191b
duffycodes <mduff001@odu.edu>
2019-02-17 16:39:35 -0600

    Added body-parser and finished making the chat buttons work

--- chat.js
+++ chat.js
@@ -3,9 +3,12 @@
  */
 
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
 	{name: 'Tim', message:'Hi'},
@@ -14,6 +17,12 @@ var messages = [
 
 app.get('/messages', (req, res)=>{
 	res.send(messages)
+})
+
+app.post('/messages', (req, res)=>{
+	//console.log(req.body)
+	messages.push(req.body)
+	res.sendStatus(200)
 })
 
 var server = app.listen(3000, ()=>{
